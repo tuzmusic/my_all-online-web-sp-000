2664f60522cec380cebe622520e78a79296a4ba0
Jonathan Tuzman <tuzmusic@gmail.com>
2018-10-10 15:52:14 +0000

    automatically backed up by learn

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -1,5 +1,11 @@
 require 'pry'
 
 def my_all?(collection)
-
+  i = 0
+  returns = []
+  while i < collection.length
+    i += 1
+    returns << yield(collection[i])
+  end
+  !returns.include?(false)
 end
